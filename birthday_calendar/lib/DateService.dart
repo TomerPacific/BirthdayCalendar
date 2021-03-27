cc61b36f0da8f249450d469b65b5a4ebf862dad0
unknown <tomerpacific@gmail.com>
2021-03-27 15:27:59 +0300

    BirthdayCalendar creating method to get number of days per month

--- birthday_calendar/lib/DateService.dart
+++ birthday_calendar/lib/DateService.dart
@@ -19,4 +19,46 @@ class DateService {
     return MONTHS[month];
   }
 
+  int amountOfDaysInMonth(String month) {
+    int days = 0;
+    switch(month) {
+      case "January":
+      case "March":
+      case "May":
+      case "July":
+      case "August":
+      case "October":
+      case "December": {
+        days = 31;
+        break;
+      }
+      case "April":
+      case "June":
+      case "September":
+      case "November": {
+        days = 30;
+        break;
+      }
+      case "February": {
+        days = isLeapYear() ? 29 : 28;
+        break;
+      }
+    }
+
+    return days;
+  }
+
+  bool isLeapYear() {
+    DateTime now = new DateTime.now();
+    int year = now.year;
+    if (year % 4 == 0 && year % 100 != 0) {
+      return true;
+    } else if (year % 4 == 0 &&
+               year % 100 == 0 &&
+              year % 400 == 0) {
+      return true;
+    }
+    return false;
+  }
+
 }
