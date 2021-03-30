2a4214af1e9728cd3a7ef4a1b89f4a3a6a4be54b
unknown <tomerpacific@gmail.com>
2021-03-30 19:50:38 +0300

    BirthdayCalendar removing logic to generate numbered days as it is not necessary

--- birthday_calendar/lib/CalendarWidget.dart
+++ birthday_calendar/lib/CalendarWidget.dart
@@ -14,18 +14,10 @@ class CalendarWidget extends StatefulWidget {
 class _CalendarState extends State<CalendarWidget> {
 
   int _amountOfDaysToPresent = 0;
-  List<String> _monthDays = [];
 
   @override void initState() {
     _amountOfDaysToPresent = DateService().amountOfDaysInMonth(DateService().convertMonthToWord(widget.currentMonth));
-    _generateMonthDays(_amountOfDaysToPresent);
     super.initState();
-  }
-
-  void _generateMonthDays(int amountOfDaysInMonth) {
-    for(int i = 0; i < amountOfDaysInMonth; i++) {
-      _monthDays.add((i+1).toString());
-    }
   }
 
   @override
