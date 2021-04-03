9a7cc6cbf8f90797dffa9b5fe7a865ed456f9336
unknown <tomerpacific@gmail.com>
2021-04-03 18:03:56 +0300

    BirthdayCalendar refactoring SharedPrefs to use factory constructor

--- birthday_calendar/lib/SharedPrefs.dart
+++ birthday_calendar/lib/SharedPrefs.dart
@@ -5,12 +5,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SharedPrefs {
   static SharedPreferences _sharedPreferences;
 
-  init() async {
+  factory SharedPrefs() => SharedPrefs._internal();
+
+  SharedPrefs._internal();
+
+  Future<void>init() async {
     if (_sharedPreferences == null) {
       _sharedPreferences = await SharedPreferences.getInstance();
     }
   }
 
 }
-
-final sharedPrefs = SharedPrefs();
