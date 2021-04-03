89abfff41d30cf2e44a19f688979ce349270ed9c
unknown <tomerpacific@gmail.com>
2021-04-04 01:01:15 +0300

    BirthdayCalendar exposing methods to get/set birthdays per day in shared prefs

--- birthday_calendar/lib/SharedPrefs.dart
+++ birthday_calendar/lib/SharedPrefs.dart
@@ -9,10 +9,18 @@ class SharedPrefs {
 
   SharedPrefs._internal();
 
-  Future<void>init() async {
+  Future<void> init() async {
     if (_sharedPreferences == null) {
       _sharedPreferences = await SharedPreferences.getInstance();
     }
+  }
+
+  List<String> getBirthdaysForDate(String date) {
+    return _sharedPreferences.getStringList(date);
+  }
+
+  void setBirthdaysForDate(String date, List<String> birthdays) {
+    _sharedPreferences.setStringList(date, birthdays);
   }
 
 }
