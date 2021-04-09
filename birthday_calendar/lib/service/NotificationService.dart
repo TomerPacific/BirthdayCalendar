f10c4b0ed3a36b4364a877d1e2161075829e8e37
unknown <tomerpacific@gmail.com>
2021-04-09 11:56:00 +0300

    BirthdayCalendar logic to initialize notifications for android

--- birthday_calendar/lib/service/NotificationService.dart
+++ birthday_calendar/lib/service/NotificationService.dart
@@ -2,13 +2,34 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationService {
-  static final NotificationService _dateService = NotificationService
+  static final NotificationService _notificationService = NotificationService
       ._internal();
 
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+
+  final AndroidInitializationSettings initializationSettingsAndroid =
+  AndroidInitializationSettings('APP_ICON');
+
+
   factory NotificationService() {
-    return _dateService;
+    return _notificationService;
   }
 
   NotificationService._internal();
+
+  Future<void> init() async {
+    final InitializationSettings initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid,
+        iOS: null,
+        macOS: null);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+    onSelectNotification: selectNotification);
+  }
+
+  Future selectNotification(String payload) async {
+
+  }
+
 
 }
