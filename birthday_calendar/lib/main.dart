3a576fee04fdf135baad58096a1adab586d83906
unknown <tomerpacific@gmail.com>
2021-04-23 17:18:28 +0300

    BirthdayCalendar changing title in app bar

--- birthday_calendar/lib/main.dart
+++ birthday_calendar/lib/main.dart
@@ -79,7 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(month),
+        title: Text(applicationName),
       ),
       body:
         GestureDetector(
