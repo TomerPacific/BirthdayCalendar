aa2124ee30e0bb94d46a23af98638a3341c15671
unknown <tomerpacific@gmail.com>
2021-12-04 01:23:50 +0200

    BirthdayCalendar converting test class to test date service and creating one unit test to check convertMonthToWord method

---
+++ test/date_service_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:birthday_calendar/service/date_service.dart';
+
+void main() {
+  test("DateService convert month number 8 to August", () {
+    final monthNumber = 8;
+    final monthName = DateService().convertMonthToWord(monthNumber);
+    expect(monthName, equals("August"));
+  });
+}
