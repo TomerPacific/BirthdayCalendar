b6bb29ca33fa95ae3a7015bd53ef538e913036db
unknown <tomerpacific@gmail.com>
2021-12-05 21:03:02 +0200

    BirthdayCalendar adding unit tests for other methods in date service

--- test/date_service_test.dart
+++ test/date_service_test.dart
@@ -3,14 +3,26 @@ import 'package:birthday_calendar/service/date_service.dart';
 
 void main() {
   test("DateService convert month number 8 to August", () {
-    final monthNumber = 8;
-    final monthName = DateService().convertMonthToWord(monthNumber);
+    final int monthNumber = 8;
+    final String monthName = DateService().convertMonthToWord(monthNumber);
     expect(monthName, equals("August"));
   });
 
   test("DateService invalid month number returns empty string", () {
-    final monthNumber = 14;
-    final monthName = DateService().convertMonthToWord(monthNumber);
+    final int monthNumber = 14;
+    final String monthName = DateService().convertMonthToWord(monthNumber);
     expect(monthName, isEmpty);
+  });
+
+  test("DateService get amount of days in month with 30 days", () {
+    final String monthName = "September";
+    final int amountOfDays = DateService().amountOfDaysInMonth(monthName);
+    expect(amountOfDays, equals(30));
+  });
+
+  test("DateService get amount of days in invalid month will be equal to zero", () {
+    final String monthName = "New Month";
+    final int amountOfDays = DateService().amountOfDaysInMonth(monthName);
+    expect(amountOfDays, equals(0));
   });
 }
