90509e12c462a3bf908e58f1dcfd8dd63072e8c8
unknown <tomerpacific@gmail.com>
2022-02-21 11:46:57 +0200

    BirthdayCalendar logic in settings screen to clear notifications

--- lib/widget/settings_screen.dart
+++ lib/widget/settings_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:birthday_calendar/service/shared_prefs.dart';
 
 class SettingsScreen extends StatefulWidget {
+  final Function onClearNotifications;
+
+  const SettingsScreen({required this.onClearNotifications});
 
   @override
   State<StatefulWidget> createState() => _SettingsScreenState();
@@ -11,6 +15,8 @@ class _SettingsScreenState extends State<SettingsScreen> {
 
   bool _isDarkModeEnabled = false;
   bool _importContacts = false;
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +41,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
               },
           ),
           CheckboxListTile(
-            title: const Text("Import Contacts?"),
+            title: const Text("Import Contacts"),
               value: _importContacts,
               onChanged: (bool? value) {
                 if (value != null) {
@@ -47,6 +53,21 @@ class _SettingsScreenState extends State<SettingsScreen> {
             secondary: const Icon(Icons.contacts,
               color: Colors.blue
             ),
+          ),
+          ListTile(
+            title: const Text("Clear Notifications"),
+            leading: const Icon(
+              Icons.clear,
+              color: Colors.redAccent),
+              onTap: () {
+                SharedPrefs().clearAllNotifications()
+                    .then((didClearAllNotifications) =>
+                {
+                  if (didClearAllNotifications) {
+                     widget.onClearNotifications()
+                  }
+                });
+              }
           )
         ],
       ),
