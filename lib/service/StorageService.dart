7d445b12381e50e7ac90a3a2f70a24c937fe7faf
unknown <tomerpacific@gmail.com>
2022-02-26 01:13:24 +0200

    BirthdayCalendar creating abstract class for a storage service and a class that extends that method for shared preferences use cases

---
+++ lib/service/StorageService.dart
@@ -0,0 +1,12 @@
+
+import 'package:birthday_calendar/model/user_birthday.dart';
+
+abstract class StorageService {
+  Future<List<UserBirthday>> getBirthdaysForDate(DateTime dateTime);
+  Future<void> saveBirthdaysForDate(DateTime dateTime, List<UserBirthday> birthdays);
+  Future<bool> clearAllBirthdays();
+  Future<void> updateNotificationStatusForBirthday(UserBirthday userBirthday, bool updatedStatus);
+
+  Future<bool> getThemeModeSetting();
+  Future<void> saveThemeModeSetting(bool isDarkModeEnabled);
+}
---
+++ lib/service/StorageServiceSharedPreferences.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+import 'package:birthday_calendar/constants.dart';
+import 'package:birthday_calendar/model/user_birthday.dart';
+import 'package:birthday_calendar/service/StorageService.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:birthday_calendar/service/date_service.dart';
+
+class StorageServiceSharedPreferences extends StorageService {
+
+  @override
+  Future<bool> clearAllBirthdays() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    return sharedPreferences.clear();
+  }
+
+  @override
+  Future<List<UserBirthday>> getBirthdaysForDate(DateTime dateTime) async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    String formattedDate = DateService().formatDateForSharedPrefs(dateTime);
+    String? birthdaysJSON = sharedPreferences.getString(formattedDate);
+    if (birthdaysJSON != null) {
+      List decodedBirthdaysForDate = jsonDecode(birthdaysJSON);
+      List<UserBirthday> birthdays = decodedBirthdaysForDate
+          .map((decodedBirthday) => UserBirthday.fromJson(decodedBirthday))
+          .toList();
+      return birthdays;
+    }
+
+    return [];
+  }
+
+  @override
+  Future<bool> getThemeModeSetting() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    bool? isDarkModeEnabled = sharedPreferences.getBool(darkModeKey);
+    return isDarkModeEnabled != null ? isDarkModeEnabled : false;
+  }
+
+  @override
+  Future<void> saveBirthdaysForDate(DateTime dateTime, List<UserBirthday> birthdays) async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    String encoded = jsonEncode(birthdays);
+    String formattedDate = DateService().formatDateForSharedPrefs(dateTime);
+    sharedPreferences.setString(formattedDate, encoded);
+  }
+
+  @override
+  Future<void> saveThemeModeSetting(bool isDarkModeEnabled) async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setBool(darkModeKey, isDarkModeEnabled);
+  }
+
+  @override
+  Future<void> updateNotificationStatusForBirthday(UserBirthday userBirthday, bool updatedStatus) async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+
+    List<UserBirthday> birthdays = await getBirthdaysForDate(userBirthday.birthdayDate);
+    for (int i = 0; i < birthdays.length; i++) {
+      UserBirthday savedBirthday = birthdays[i];
+      if (savedBirthday.equals(userBirthday)) {
+        savedBirthday.updateNotificationStatus(updatedStatus);
+      }
+    }
+  }
+
+}
