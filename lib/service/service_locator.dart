7a70ad05867ce87ce36bafb1bd71920b35ad924f
unknown <tomerpacific@gmail.com>
2022-03-01 23:25:18 +0200

    BirthdayCalendar exposing permissions service in service locator

--- lib/service/service_locator.dart
+++ lib/service/service_locator.dart
@@ -1,3 +1,4 @@
+
 import 'storage_service/storage_service.dart';
 import 'storage_service/shared_preferences_storage.dart';
 import 'date_service/date_service.dart';
@@ -5,6 +6,8 @@ import 'date_service/date_service_impl.dart';
 import 'notification_service/notification_service_impl.dart';
 import 'notification_service/notification_service.dart';
 import 'package:birthday_calendar/pages/settings_page/settings_screen_manager.dart';
+import 'package:birthday_calendar/service/permission_service/permissions_service.dart';
+import 'package:birthday_calendar/service/permission_service/permissions_service_impl.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
@@ -13,5 +16,6 @@ setupServiceLocator() {
   getIt.registerLazySingleton<DateService>(() => DateServiceImpl());
   getIt.registerLazySingleton<StorageService>(() => StorageServiceSharedPreferences());
   getIt.registerLazySingleton<NotificationService>(() => NotificationServiceImpl());
+  getIt.registerLazySingleton<PermissionsService>(() => PermissionsServiceImpl());
   getIt.registerLazySingleton<SettingsScreenManager>(() => SettingsScreenManager());
 }
