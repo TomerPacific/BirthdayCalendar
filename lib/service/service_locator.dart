4e213e34a51dfde02f75a565274628032c2e1807
unknown <tomerpacific@gmail.com>
2022-03-01 15:10:14 +0200

    BirthdayCalendar adding settings screen manager to service locator

--- lib/service/service_locator.dart
+++ lib/service/service_locator.dart
@@ -4,8 +4,8 @@ import 'package:birthday_calendar/service/date_service.dart';
 import 'package:birthday_calendar/service/date_service_impl.dart';
 import 'package:birthday_calendar/service/notification_service_impl.dart';
 import 'package:birthday_calendar/service/notification_service.dart';
+import 'package:birthday_calendar/widget/settings_screen_manager.dart';
 import 'package:get_it/get_it.dart';
-
 
 final getIt = GetIt.instance;
 
@@ -13,4 +13,5 @@ setupServiceLocator() {
   getIt.registerLazySingleton<DateService>(() => DateServiceImpl());
   getIt.registerLazySingleton<StorageService>(() => StorageServiceSharedPreferences());
   getIt.registerLazySingleton<NotificationService>(() => NotificationServiceImpl());
+  getIt.registerLazySingleton<SettingsScreenManager>(() => SettingsScreenManager());
 }
