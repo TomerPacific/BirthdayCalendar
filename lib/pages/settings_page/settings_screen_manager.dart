c7f030cca631abb6d3720340a961c8e29a71b6e2
unknown <tomerpacific@gmail.com>
2022-03-06 12:51:43 +0200

    BirthdayCalendar using contacts service where relevant

--- lib/pages/settings_page/settings_screen_manager.dart
+++ lib/pages/settings_page/settings_screen_manager.dart
@@ -1,25 +1,22 @@
 
 import 'dart:async';
-
+import 'package:tuple/tuple.dart';
 import 'package:birthday_calendar/pages/settings_page/notifiers/ClearBirthdaysNotifier.dart';
 import 'package:birthday_calendar/pages/settings_page/notifiers/ImportContactsNotifier.dart';
 import 'package:birthday_calendar/pages/settings_page/notifiers/VersionNotifier.dart';
+import 'package:birthday_calendar/service/contacts_service/bc_contacts_service.dart';
 import 'package:birthday_calendar/service/permission_service/permissions_service.dart';
 import 'notifiers/ThemeChangeNotifier.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:birthday_calendar/constants.dart';
 import 'package:contacts_service/contacts_service.dart';
-import 'package:birthday_calendar/model/user_birthday.dart';
-import 'package:birthday_calendar/service/notification_service/notification_service.dart';
-import 'package:birthday_calendar/service/storage_service/storage_service.dart';
 import 'package:birthday_calendar/service/service_locator.dart';
-import 'package:tuple/tuple.dart';
+
 
 class SettingsScreenManager {
 
   final PermissionsService _permissionsService = getIt<PermissionsService>();
-  final StorageService _storageService = getIt<StorageService>();
-  final NotificationService _notificationService = getIt<NotificationService>();
+  final BCContactsService _bcContactsService = getIt<BCContactsService>();
   final ThemeChangeNotifier themeChangeNotifier = ThemeChangeNotifier();
   final VersionNotifier versionNotifier = VersionNotifier();
   final ClearBirthdaysNotifier clearBirthdaysNotifier = ClearBirthdaysNotifier();
@@ -41,8 +38,9 @@ class SettingsScreenManager {
     } else if (status == PermissionStatus.permanentlyDenied) {
         importContactsNotifier.toggleImportContacts();
     } else if (status == PermissionStatus.granted) {
-      List<Contact> contacts = await ContactsService.getContacts(withThumbnails: false);
-      List<Contact> contactsWithoutBirthDates = await _addBirthdaysOfContactsAndSetNotifications(contacts);
+      List<Contact> contacts = await _bcContactsService.fetchContacts(false);
+      List<Contact> contactsWithoutBirthDates = await _bcContactsService.gatherContactsWithoutBirthdays(contacts);
+      _bcContactsService.addContactsWithBirthdays(contacts);
       pair = Tuple2(status, contactsWithoutBirthDates);
     }
 
@@ -53,8 +51,9 @@ class SettingsScreenManager {
     PermissionStatus status = await _permissionsService.requestPermissionAndGetStatus(contactsPermissionKey);
     List<Contact> contactsWithoutBirthDates = [];
     if (status == PermissionStatus.granted) {
-      List<Contact> contacts = await ContactsService.getContacts(withThumbnails: false);
-      contactsWithoutBirthDates = await _addBirthdaysOfContactsAndSetNotifications(contacts);
+      List<Contact> contacts = await _bcContactsService.fetchContacts(false);
+      contactsWithoutBirthDates = await _bcContactsService.gatherContactsWithoutBirthdays(contacts);
+      _bcContactsService.addContactsWithBirthdays(contacts);
     } else if (status == PermissionStatus.permanentlyDenied) {
       importContactsNotifier.toggleImportContacts();
     }
@@ -62,38 +61,7 @@ class SettingsScreenManager {
     return Tuple2(status, contactsWithoutBirthDates);
   }
 
-  Future<List<Contact>> _addBirthdaysOfContactsAndSetNotifications(List<Contact> contacts) async {
-    List<Contact> usersWithoutBirthdays = [];
-    for (Contact person in contacts) {
-      if (person.birthday == null) {
-        usersWithoutBirthdays.add(person);
-      } else if (person.birthday != null &&
-          person.displayName != null &&
-          person.phones != null) {
-        addContactToCalendar(person);
-      }
-    }
-    
-    return usersWithoutBirthdays;
-  }
-
-  void addContactToCalendar(Contact contact) async {
-    String phoneNumber = "";
-    if (contact.phones != null && contact.phones!.length > 0) {
-      phoneNumber = contact.phones!.first.value!;
-    }
-
-    UserBirthday birthday = new UserBirthday(
-        contact.displayName!,
-        contact.birthday!,
-        false,
-        phoneNumber);
-    _notificationService.scheduleNotificationForBirthday(
-        birthday, "${contact.displayName!} has an upcoming birthday!");
-    List<UserBirthday> birthdays = await _storageService
-        .getBirthdaysForDate(contact.birthday!, false);
-    birthdays.add(birthday);
-
-    _storageService.saveBirthdaysForDate(contact.birthday!, birthdays);
+  void addContactToCalendar(Contact contact) {
+    _bcContactsService.addContactToCalendar(contact);
   }
 }
