4d5fa775822d6027bd474c72f040d826e5ce9ad6
unknown <tomerpacific@gmail.com>
2022-03-06 12:51:20 +0200

    BirthdayCalendar adding contacts service to service locator

--- lib/service/service_locator.dart
+++ lib/service/service_locator.dart
@@ -1,13 +1,16 @@
 
+
 import 'storage_service/storage_service.dart';
 import 'storage_service/shared_preferences_storage.dart';
 import 'date_service/date_service.dart';
 import 'date_service/date_service_impl.dart';
 import 'notification_service/notification_service_impl.dart';
 import 'notification_service/notification_service.dart';
+import 'package:birthday_calendar/service/contacts_service/bc_contacts_service.dart';
 import 'package:birthday_calendar/pages/settings_page/settings_screen_manager.dart';
 import 'package:birthday_calendar/service/permission_service/permissions_service.dart';
 import 'package:birthday_calendar/service/permission_service/permissions_service_impl.dart';
+import 'package:birthday_calendar/service/contacts_service/bc_contacts_service_impl.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
@@ -18,4 +21,5 @@ setupServiceLocator() {
   getIt.registerLazySingleton<NotificationService>(() => NotificationServiceImpl());
   getIt.registerLazySingleton<PermissionsService>(() => PermissionsServiceImpl());
   getIt.registerLazySingleton<SettingsScreenManager>(() => SettingsScreenManager());
+  getIt.registerLazySingleton<BCContactsService>(() => BCContactsServiceImpl());
 }
