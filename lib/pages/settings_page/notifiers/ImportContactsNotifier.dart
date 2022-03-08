7bb610f7853e55c98ef8bffa8b096f916a1679d4
unknown <tomerpacific@gmail.com>
2022-03-08 14:26:41 +0200

    BirthdayCalendar logic to set value of notifier

--- lib/pages/settings_page/notifiers/ImportContactsNotifier.dart
+++ lib/pages/settings_page/notifiers/ImportContactsNotifier.dart
@@ -1,8 +1,19 @@
 
+import 'package:birthday_calendar/service/storage_service/storage_service.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:birthday_calendar/service/service_locator.dart';
 
 class ImportContactsNotifier extends ValueNotifier<bool> {
-  ImportContactsNotifier() : super(false);
+  ImportContactsNotifier() : super(false) {
+    _getImportContactsStatus();
+  }
+
+  StorageService _storageService = getIt<StorageService>();
+
+  void _getImportContactsStatus() async {
+    bool isContactsPermissionPermanentlyDenied = await _storageService.getContactsPermissionStatus();
+    value = isContactsPermissionPermanentlyDenied;
+  }
 
   void toggleImportContacts() {
     value = !value;
