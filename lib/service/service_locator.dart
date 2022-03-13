64d25696dc3f1db83fc18708d8ed4aecdb7b455d
unknown <tomerpacific@gmail.com>
2022-03-13 10:40:01 +0200

    BirthdayCalendar adding new service to service locator

--- lib/service/service_locator.dart
+++ lib/service/service_locator.dart
@@ -1,5 +1,7 @@
 import 'package:birthday_calendar/service/StorageService.dart';
 import 'package:birthday_calendar/service/StorageServiceSharedPreferences.dart';
+import 'package:birthday_calendar/service/VersionSpecificService.dart';
+import 'package:birthday_calendar/service/VersionSpecificServiceImpl.dart';
 import 'package:birthday_calendar/service/date_service.dart';
 import 'package:birthday_calendar/service/date_service_impl.dart';
 import 'package:birthday_calendar/service/notification_service_impl.dart';
@@ -13,4 +15,5 @@ setupServiceLocator() {
   getIt.registerLazySingleton<DateService>(() => DateServiceImpl());
   getIt.registerLazySingleton<StorageService>(() => StorageServiceSharedPreferences());
   getIt.registerLazySingleton<NotificationService>(() => NotificationServiceImpl());
+  getIt.registerLazySingleton<VersionSpecificService>(() => VersionSpecificServiceImpl());
 }
