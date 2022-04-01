9189c43166adf4ff66279e99b7d707c49db5e9e5
unknown <tomerpacific@gmail.com>
2022-04-01 10:46:30 +0300

    BirthdayCalendar fixing usage of Provider.of by adding type and logic to handle status change of notification for birthday

--- lib/page/birthday/birthday.dart
+++ lib/page/birthday/birthday.dart
@@ -1,11 +1,9 @@
 import 'package:birthday_calendar/page/birthday/birthday_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:birthday_calendar/model/user_birthday.dart';
-import '../../service/storage_service/storage_service.dart';
-import '../../service/notification_service/notification_service.dart';
-import 'package:birthday_calendar/service/service_locator.dart';
+import 'package:provider/provider.dart';
 
-class BirthdayWidget extends StatefulWidget {
+class BirthdayWidget extends StatelessWidget {
   final UserBirthday birthdayOfPerson;
   final VoidCallback onDeletePressedCallback;
   final int indexOfBirthday;
@@ -17,72 +15,50 @@ class BirthdayWidget extends StatefulWidget {
       required this.indexOfBirthday})
       : super(key: key);
 
-  @override
-  _BirthdayWidgetState createState() => _BirthdayWidgetState();
-}
 
-class _BirthdayWidgetState extends State<BirthdayWidget> {
-  bool isNotificationEnabledForPerson = false;
-  StorageService _storageService = getIt<StorageService>();
-  NotificationService _notificationService = getIt<NotificationService>();
-  BirthdayManager _birthdayManager = new BirthdayManager();
-
-
-  void updateNotificationStatusForBirthday() {
-    setState(() {
-      isNotificationEnabledForPerson = !isNotificationEnabledForPerson;
-    });
-    _storageService.updateNotificationStatusForBirthday(
-        widget.birthdayOfPerson, isNotificationEnabledForPerson);
-    if (!isNotificationEnabledForPerson) {
-      _notificationService.cancelNotificationForBirthday(widget.birthdayOfPerson);
-    } else {
-      _notificationService.scheduleNotificationForBirthday(
-          widget.birthdayOfPerson,
-          "${widget.birthdayOfPerson.name} has an upcoming birthday!");
-    }
-  }
-
-  @override
-  void initState() {
-    isNotificationEnabledForPerson = widget.birthdayOfPerson.hasNotification;
-    super.initState();
-  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 40,
-      color: _birthdayManager.getColorBasedOnPosition(widget.indexOfBirthday, "background"),
-      child: Row(
-        children: [
-          new Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              widget.birthdayOfPerson.name,
-              textDirection: TextDirection.ltr,
-              style: new TextStyle(fontSize: 20.0, color: _birthdayManager.getColorBasedOnPosition(widget.indexOfBirthday, "text")),
+    return ChangeNotifierProvider(
+        create:  (context) => BirthdayManager(birthdayOfPerson),
+        builder: (context, provider) {
+          return Container(
+            height: 40,
+            color: Provider.of<BirthdayManager>(context, listen: false).getColorBasedOnPosition(indexOfBirthday, "background"),
+            child: Row(
+              children: [
+                new Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    birthdayOfPerson.name,
+                    textDirection: TextDirection.ltr,
+                    style: new TextStyle(fontSize: 20.0, color: Provider.of<BirthdayManager>(context, listen: false).getColorBasedOnPosition(indexOfBirthday, "text")),
+                  ),
+                ),
+                new Spacer(),
+                new Consumer<BirthdayManager>(
+                  builder: (context, data, child) =>
+                  new IconButton(
+                      icon: Icon(
+                          !data.userBirthday.hasNotification
+                              ? Icons.notifications_off_outlined
+                              : Icons.notifications_active_outlined,
+                          color:  Provider.of<BirthdayManager>(context, listen: false).getColorBasedOnPosition(indexOfBirthday, "icon")),
+                      onPressed: () {
+                        Provider.of<BirthdayManager>(context, listen: false).updateNotificationStatusForBirthday();
+                      }),
+                ),
+                new IconButton(
+                    icon: Icon(Icons.call, color: Provider.of<BirthdayManager>(context, listen: false).getColorBasedOnPosition(indexOfBirthday, "icon")),
+                    onPressed: () {
+                      Provider.of<BirthdayManager>(context, listen: false).handleCallButtonPressed(birthdayOfPerson.phoneNumber);
+                    }),
+                new IconButton(
+                    icon: Icon(Icons.clear, color: Provider.of<BirthdayManager>(context, listen: false).getColorBasedOnPosition(indexOfBirthday, "icon")),
+                    onPressed: onDeletePressedCallback),
+              ],
             ),
-          ),
-          new Spacer(),
-          new IconButton(
-              icon: Icon(
-                  !isNotificationEnabledForPerson
-                      ? Icons.notifications_off_outlined
-                      : Icons.notifications_active_outlined,
-                  color:  _birthdayManager.getColorBasedOnPosition(widget.indexOfBirthday, "icon")),
-              onPressed: () {
-                updateNotificationStatusForBirthday();
-              }),
-          new IconButton(
-              icon: Icon(Icons.call, color: _birthdayManager.getColorBasedOnPosition(widget.indexOfBirthday, "icon")),
-              onPressed: () {
-                _birthdayManager.handleCallButtonPressed(widget.birthdayOfPerson.phoneNumber);
-              }),
-          new IconButton(
-              icon: Icon(Icons.clear, color: _birthdayManager.getColorBasedOnPosition(widget.indexOfBirthday, "icon")),
-              onPressed: widget.onDeletePressedCallback),
-        ],
-      ),
+          );
+        },
     );
   }
 }
--- lib/page/birthday/birthday_manager.dart
+++ lib/page/birthday/birthday_manager.dart
@@ -1,8 +1,21 @@
+import 'package:birthday_calendar/model/user_birthday.dart';
+import 'package:birthday_calendar/service/notification_service/notification_service.dart';
+import 'package:birthday_calendar/service/service_locator.dart';
+import 'package:birthday_calendar/service/storage_service/storage_service.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class BirthdayManager {
+class BirthdayManager extends ChangeNotifier {
 
+  StorageService _storageService = getIt<StorageService>();
+  NotificationService _notificationService = getIt<NotificationService>();
+  UserBirthday _userBirthday = new UserBirthday("", DateTime.now(), false, "");
+
+  UserBirthday get userBirthday => _userBirthday;
+
+  BirthdayManager(UserBirthday birthday) {
+    _userBirthday = birthday;
+  }
 
   Color getColorBasedOnPosition(int index, String element) {
     if (element == "background") {
@@ -17,6 +30,21 @@ class BirthdayManager {
     if (await canLaunch(phoneUrl)) {
       launch(phoneUrl);
     }
+  }
+
+  void updateNotificationStatusForBirthday() {
+    bool isNotificationEnabledForPerson = _userBirthday.hasNotification;
+    isNotificationEnabledForPerson = !isNotificationEnabledForPerson;
+    userBirthday.hasNotification = isNotificationEnabledForPerson;
+    _storageService.updateNotificationStatusForBirthday(_userBirthday, isNotificationEnabledForPerson);
+    if (!isNotificationEnabledForPerson) {
+      _notificationService.cancelNotificationForBirthday(_userBirthday);
+    } else {
+      _notificationService.scheduleNotificationForBirthday(
+          _userBirthday,
+          "${_userBirthday.name} has an upcoming birthday!");
+    }
+    notifyListeners();
   }
 
 }
