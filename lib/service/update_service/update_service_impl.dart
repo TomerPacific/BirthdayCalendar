d9e9e01c3bb4095d79c46025e6ac4077eac6f328
unknown <tomerpacific@gmail.com>
2022-06-11 15:08:56 +0300

    BirthdayCalendar creating method to initialize appUpdateInfo and removing all checks

--- lib/service/update_service/update_service_impl.dart
+++ lib/service/update_service/update_service_impl.dart
@@ -5,29 +5,24 @@ import 'package:in_app_update/in_app_update.dart';
 
 class UpdateServiceImpl extends UpdateService {
 
-  AppUpdateInfo? _appUpdateInfo;
+  late AppUpdateInfo _appUpdateInfo;
+
+  void init() async {
+    _appUpdateInfo = await InAppUpdate.checkForUpdate();
+  }
 
   @override
   Future<bool> isUpdateAvailable() async {
-      _appUpdateInfo = await InAppUpdate.checkForUpdate();
-      return _appUpdateInfo!.updateAvailability == UpdateAvailability.updateAvailable;
+      return _appUpdateInfo.updateAvailability == UpdateAvailability.updateAvailable;
   }
   @override
   Future<bool> isImmediateUpdatePossible() async {
-    if (_appUpdateInfo == null) {
-      _appUpdateInfo = await InAppUpdate.checkForUpdate();
-    }
-
-    return _appUpdateInfo!.immediateUpdateAllowed;
+    return _appUpdateInfo.immediateUpdateAllowed;
   }
 
   @override
   Future<bool> isFlexibleUpdatePossible() async {
-    if (_appUpdateInfo == null) {
-      _appUpdateInfo = await InAppUpdate.checkForUpdate();
-    }
-
-    return _appUpdateInfo!.flexibleUpdateAllowed;
+    return _appUpdateInfo.flexibleUpdateAllowed;
   }
 
 
