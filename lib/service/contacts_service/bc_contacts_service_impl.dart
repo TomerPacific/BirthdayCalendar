ecf37ba9464dcabf87c5ab16b17f5aa6e9d6b0be
unknown <tomerpacific@gmail.com>
2024-04-18 23:19:35 +0300

    BirthdayCalendar refactoring contacts service due to new library

--- lib/service/contacts_service/bc_contacts_service_impl.dart
+++ lib/service/contacts_service/bc_contacts_service_impl.dart
@@ -1,10 +1,10 @@
-import 'package:contacts_service/contacts_service.dart';
 import 'package:birthday_calendar/service/contacts_service/bc_contacts_service.dart';
 import 'package:birthday_calendar/service/service_locator.dart';
 import 'package:birthday_calendar/service/storage_service/storage_service.dart';
 import 'package:birthday_calendar/service/notification_service/notification_service.dart';
 import 'package:birthday_calendar/model/user_birthday.dart';
 import 'package:collection/collection.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
 
 class BCContactsServiceImpl extends BCContactsService {
 
@@ -13,28 +13,12 @@ class BCContactsServiceImpl extends BCContactsService {
 
   @override
   Future<List<Contact>> fetchContacts(bool withThumbnails) async {
-    return await ContactsService.getContacts(withThumbnails: false);
+    return await FlutterContacts.getContacts();
   }
 
-  Future<List<Contact>> gatherContactsWithoutBirthdays(List<Contact> contacts) async {
-    List<Contact> usersWithoutBirthdays = contacts.where((element) => element.birthday == null).toList();
-    return usersWithoutBirthdays;
-  }
-
-  void addContactsWithBirthdays(List<Contact> contacts) {
-    for (Contact person in contacts) {
-      if (person.birthday != null &&
-          person.displayName != null &&
-          person.phones != null) {
-        addContactToCalendar(person);
-      }
-    }
-  }
-
-  void addContactToCalendar(Contact contact) async {
-    String phoneNumber = "";
-    List<UserBirthday> birthdays = await _storageService.getBirthdaysForDate(contact.birthday!, false);
-    String contactName = contact.displayName!;
+  void addContactToCalendar(UserBirthday contact) async {
+    List<UserBirthday> birthdays = await _storageService.getBirthdaysForDate(contact.birthdayDate, false);
+    String contactName = contact.name;
 
     UserBirthday? birthdayWithSameName = birthdays.firstWhereOrNull((element) => element.name == contactName);
 
@@ -43,21 +27,12 @@ class BCContactsServiceImpl extends BCContactsService {
       return;
     }
 
-    if (contact.phones != null && contact.phones!.length > 0) {
-      phoneNumber = contact.phones!.first.value!;
-    }
-
-    UserBirthday birthday = new UserBirthday(
-        contact.displayName!,
-        contact.birthday!,
-        true,
-        phoneNumber);
     _notificationService.scheduleNotificationForBirthday(
-        birthday, "${contact.displayName!} has an upcoming birthday!");
+        contact, "${contact.name} has an upcoming birthday!");
 
-    birthdays.add(birthday);
+    birthdays.add(contact);
 
-    _storageService.saveBirthdaysForDate(contact.birthday!, birthdays);
+    _storageService.saveBirthdaysForDate(contact.birthdayDate, birthdays);
   }
 
 }
