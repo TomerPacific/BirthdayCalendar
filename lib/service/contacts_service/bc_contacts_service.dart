f5b1cf666600311d7def03442ebb14591876a404
unknown <tomerpacific@gmail.com>
2024-04-26 11:35:28 +0300

    BirthdayCalendar moving logic to request permission from contacts service

--- lib/service/contacts_service/bc_contacts_service.dart
+++ lib/service/contacts_service/bc_contacts_service.dart
@@ -1,7 +1,9 @@
 import 'package:birthday_calendar/model/user_birthday.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 abstract class BCContactsService {
   Future<List<Contact>> fetchContacts(bool withThumbnails);
   void addContactToCalendar(UserBirthday contact);
+  Future<PermissionStatus> requestContactsPermission();
 }
--- lib/service/contacts_service/bc_contacts_service_impl.dart
+++ lib/service/contacts_service/bc_contacts_service_impl.dart
@@ -1,21 +1,26 @@
+import 'package:birthday_calendar/constants.dart';
 import 'package:birthday_calendar/service/contacts_service/bc_contacts_service.dart';
+import 'package:birthday_calendar/service/permission_service/permissions_service.dart';
 import 'package:birthday_calendar/service/service_locator.dart';
 import 'package:birthday_calendar/service/storage_service/storage_service.dart';
 import 'package:birthday_calendar/service/notification_service/notification_service.dart';
 import 'package:birthday_calendar/model/user_birthday.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class BCContactsServiceImpl extends BCContactsService {
 
   final StorageService _storageService = getIt<StorageService>();
   final NotificationService _notificationService = getIt<NotificationService>();
+  final PermissionsService _permissionsService = getIt<PermissionsService>();
 
   @override
   Future<List<Contact>> fetchContacts(bool withThumbnails) async {
     return await FlutterContacts.getContacts(withThumbnail: withThumbnails);
   }
 
+  @override
   void addContactToCalendar(UserBirthday contact) async {
     List<UserBirthday> birthdays = await _storageService.getBirthdaysForDate(contact.birthdayDate, false);
     String contactName = contact.name;
@@ -33,6 +38,11 @@ class BCContactsServiceImpl extends BCContactsService {
     birthdays.add(contact);
 
     _storageService.saveBirthdaysForDate(contact.birthdayDate, birthdays);
+  }
+
+  @override
+  Future<PermissionStatus> requestContactsPermission() async {
+    return await _permissionsService.getPermissionStatus(contactsPermissionKey);
   }
 
 }
