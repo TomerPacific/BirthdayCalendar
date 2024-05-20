0bbdad0de886628d6acf3f7ac1e5e6a5a0cad789
unknown <tomerpacific@gmail.com>
2024-05-20 20:44:52 +0300

    BirthdayCalendar converting main dart to use cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 
+import 'package:birthday_calendar/ThemeCubit.dart';
 import 'package:birthday_calendar/service/service_locator.dart';
-import 'package:birthday_calendar/page/settings_page/settings_screen_manager.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
 import 'constants.dart';
 import 'service/date_service/date_service.dart';
 import 'package:birthday_calendar/page/main_page/main_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 final DateService _dateService = getIt<DateService>();
 
@@ -20,25 +19,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-        return
-          ChangeNotifierProvider(
-            create: (context) => SettingsScreenManager(),
-              builder: (context, provider) {
-                return Consumer<SettingsScreenManager>(
-                  builder: (context, notifier, child) {
-                    return MaterialApp(
-                      title: applicationName,
-                      theme: ThemeData(),
-                      darkTheme: ThemeData.dark(),
-                      themeMode: notifier.themeMode,
-                      home: MainPage(
-                          key: Key("BirthdayCalendar"),
-                          title: applicationName,
-                          currentMonth: _dateService.getCurrentMonthNumber()
-                      ),
-                    );
-                  }
-                );
-          });
-        }
+    return BlocProvider(
+      create: (_) => ThemeCubit(),
+      child: BlocBuilder<ThemeCubit, ThemeData>(
+        builder: (_, theme) {
+          return MaterialApp(
+            title: applicationName,
+            theme: theme,
+            home: MainPage(
+                key: Key("BirthdayCalendar"),
+                title: applicationName,
+                currentMonth: _dateService.getCurrentMonthNumber()
+            )
+          );
+        },
+      )
+    );
+  }
 }
