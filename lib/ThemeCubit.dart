d2c1f18a7988059a14659ae6a19838aeacef8305
unknown <tomerpacific@gmail.com>
2024-05-20 20:44:27 +0300

    BirthdayCalendar creating theme cubit

---
+++ lib/ThemeCubit.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ThemeCubit extends Cubit<ThemeData> {
+  /// {@macro brightness_cubit}
+  ThemeCubit() : super(_lightTheme);
+
+  static final _lightTheme = ThemeData.light();
+
+  static final _darkTheme = ThemeData.dark();
+
+  /// Toggles the current brightness between light and dark.
+  void toggleTheme() {
+    emit(state.brightness == Brightness.dark ? _lightTheme : _darkTheme);
+  }
+}
