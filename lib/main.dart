a7d42b3eb354a03c5c77fba74c6e949851ce13cc
unknown <tomerpacific@gmail.com>
2024-05-24 10:00:28 +0300

    BirthdayCalendar using theme from bloc in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:birthday_calendar/ThemeBloc.dart';
 import 'package:birthday_calendar/ThemeCubit.dart';
 import 'package:birthday_calendar/service/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'ThemeState.dart';
 import 'constants.dart';
 import 'service/date_service/date_service.dart';
 import 'package:birthday_calendar/page/main_page/main_screen.dart';
@@ -18,17 +20,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-          return MaterialApp(
-            title: applicationName,
-            theme: ThemeData.light(),
-            home: BlocProvider<ThemeCubit>(
-                  create: (_) => ThemeCubit(),
+      return BlocProvider(
+        create: (context) => ThemeBloc(),
+        child: BlocBuilder<ThemeBloc, ThemeState>(
+          builder: (context, state) {
+            return MaterialApp(
+                title: applicationName,
+                theme: state.themeData,
+                home: BlocProvider<ThemeCubit>(
+                create: (_) => ThemeCubit(),
                   child: MainPage(
-                      key: Key("BirthdayCalendar"),
-                      title: applicationName,
-                      currentMonth: _dateService.getCurrentMonthNumber()
-                  )
-            )
-          );
-        }
+                  key: Key("BirthdayCalendar"),
+                  title: applicationName,
+                  currentMonth: _dateService.getCurrentMonthNumber()
+                )
+              )
+            );
+          },
+        ),
+      );
+      }
     }
