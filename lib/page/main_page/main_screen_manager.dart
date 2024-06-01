d4f832e50b3586a506763e2fad85eac45be9a3b8
unknown <tomerpacific@gmail.com>
2024-06-01 22:15:11 +0300

    BirthdayCalendar removing usage of get it and relying on services passed as arguments

--- lib/page/main_page/main_screen_manager.dart
+++ lib/page/main_page/main_screen_manager.dart
@@ -1,19 +1,18 @@
 
-import 'package:birthday_calendar/service/service_locator.dart';
 import 'package:birthday_calendar/service/storage_service/storage_service.dart';
 import 'package:birthday_calendar/service/version_specific_service/VersionSpecificService.dart';
 
 class MainScreenManager {
 
-  int monthToPresent = -1;
+  final StorageService storageService;
+  final VersionSpecificService versionSpecificService;
 
-  StorageService _storageService = getIt<StorageService>();
-  VersionSpecificService _versionSpecificService = getIt<VersionSpecificService>();
+  MainScreenManager(this.storageService, this.versionSpecificService);
 
   void makeVersionAdjustments() async {
-    bool didAlreadyMigrateNotificationStatus = await _storageService.getAlreadyMigrateNotificationStatus();
+    bool didAlreadyMigrateNotificationStatus = await storageService.getAlreadyMigrateNotificationStatus();
     if (!didAlreadyMigrateNotificationStatus) {
-      _versionSpecificService.migrateNotificationStatus();
+      versionSpecificService.migrateNotificationStatus();
     }
   }
 
