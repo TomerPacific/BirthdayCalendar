2a3880b52f570ddb97414395cef61823e56e33e1
unknown <tomerpacific@gmail.com>
2024-06-02 19:23:48 +0300

    BirthdayCalendar removing services from service locator

--- lib/service/service_locator.dart
+++ lib/service/service_locator.dart
@@ -1,28 +1,17 @@
-
-import 'storage_service/storage_service.dart';
-import 'storage_service/shared_preferences_storage.dart';
-import 'date_service/date_service.dart';
-import 'date_service/date_service_impl.dart';
 import 'notification_service/notification_service_impl.dart';
 import 'notification_service/notification_service.dart';
 import 'package:birthday_calendar/page/settings_page/settings_screen_manager.dart';
 import 'package:birthday_calendar/service/permission_service/permissions_service.dart';
 import 'package:birthday_calendar/service/permission_service/permissions_service_impl.dart';
-import 'version_specific_service/VersionSpecificService.dart';
-import 'version_specific_service/VersionSpecificServiceImpl.dart';
 import 'package:birthday_calendar/service/update_service/update_service.dart';
 import 'package:birthday_calendar/service/update_service/update_service_impl.dart';
-
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
 setupServiceLocator() {
-  getIt.registerLazySingleton<DateService>(() => DateServiceImpl());
-  getIt.registerLazySingleton<StorageService>(() => StorageServiceSharedPreferences());
   getIt.registerLazySingleton<NotificationService>(() => NotificationServiceImpl());
   getIt.registerLazySingleton<PermissionsService>(() => PermissionsServiceImpl());
   getIt.registerLazySingleton<SettingsScreenManager>(() => SettingsScreenManager());
-  getIt.registerLazySingleton<VersionSpecificService>(() => VersionSpecificServiceImpl());
   getIt.registerLazySingleton<UpdateService>(() => UpdateServiceImpl());
 }
