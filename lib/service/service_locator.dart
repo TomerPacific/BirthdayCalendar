6a9b0240db730eb33afc0bd0e5c563a3a6e1db48
unknown <tomerpacific@gmail.com>
2024-06-02 22:19:48 +0300

    BirthdayCalendar removing settings screen manager

--- lib/service/service_locator.dart
+++ lib/service/service_locator.dart
@@ -1,6 +1,5 @@
 import 'notification_service/notification_service_impl.dart';
 import 'notification_service/notification_service.dart';
-import 'package:birthday_calendar/page/settings_page/settings_screen_manager.dart';
 import 'package:birthday_calendar/service/permission_service/permissions_service.dart';
 import 'package:birthday_calendar/service/permission_service/permissions_service_impl.dart';
 import 'package:birthday_calendar/service/update_service/update_service.dart';
@@ -12,6 +11,5 @@ final getIt = GetIt.instance;
 setupServiceLocator() {
   getIt.registerLazySingleton<NotificationService>(() => NotificationServiceImpl());
   getIt.registerLazySingleton<PermissionsService>(() => PermissionsServiceImpl());
-  getIt.registerLazySingleton<SettingsScreenManager>(() => SettingsScreenManager());
   getIt.registerLazySingleton<UpdateService>(() => UpdateServiceImpl());
 }
