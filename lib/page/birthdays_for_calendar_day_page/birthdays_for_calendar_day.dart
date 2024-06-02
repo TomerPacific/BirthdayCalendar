075e6ddb4bd5f22c9847399a959cdf0edbaba0a2
unknown <tomerpacific@gmail.com>
2024-06-02 19:20:00 +0300

    BirthdayCalendar passing services to birthdays for calendar day widget

--- lib/page/birthdays_for_calendar_day_page/birthdays_for_calendar_day.dart
+++ lib/page/birthdays_for_calendar_day_page/birthdays_for_calendar_day.dart
@@ -1,33 +1,44 @@
 
 import 'package:birthday_calendar/page/birthdays_for_calendar_day_page/birthdays_for_calendar_day_manager.dart';
+import 'package:birthday_calendar/service/notification_service/notification_service.dart';
+import 'package:birthday_calendar/service/storage_service/storage_service.dart';
 import 'package:flutter/material.dart';
 import 'package:birthday_calendar/page/birthday/birthday.dart';
 import 'package:birthday_calendar/model/user_birthday.dart';
-import 'package:birthday_calendar/service/service_locator.dart';
 import 'package:birthday_calendar/service/date_service/date_service.dart';
 import 'package:provider/provider.dart';
 
 class BirthdaysForCalendarDayWidget extends StatelessWidget {
   final DateTime dateOfDay;
   final List<UserBirthday> birthdays;
+  final DateService dateService;
+  final StorageService storageService;
+  final NotificationService notificationService;
 
-  BirthdaysForCalendarDayWidget(
-      {required Key key, required this.dateOfDay, required this.birthdays})
+  BirthdaysForCalendarDayWidget({
+        required Key key,
+        required this.dateOfDay,
+        required this.birthdays,
+        required this.dateService,
+        required this.storageService,
+        required this.notificationService})
       : super(key: key);
-
-  final DateService _dateService = getIt<DateService>();
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => BirthdaysForCalendarDayManager(this.birthdays, this.dateOfDay),
+      create: (context) => BirthdaysForCalendarDayManager(
+          this.birthdays,
+          this.dateOfDay,
+          notificationService,
+          storageService),
           builder: (context, provider) {
               return Scaffold(
               appBar: AppBar(
               title: FittedBox(
                   fit: BoxFit.fitWidth,
                   child: Text(
-                      "Birthdays for ${_dateService.convertMonthToWord(this.dateOfDay.month)} ${this.dateOfDay.day}")
+                      "Birthdays for ${dateService.convertMonthToWord(this.dateOfDay.month)} ${this.dateOfDay.day}")
               )
           ),
             body: Center(
@@ -45,7 +56,9 @@ class BirthdaysForCalendarDayWidget extends StatelessWidget {
                                       onDeletePressedCallback: () {
                                         Provider.of<BirthdaysForCalendarDayManager>(context, listen: false).removeBirthdayFromList(data.birthdays[index]);
                                     },
-                                    indexOfBirthday: index);
+                                    indexOfBirthday: index,
+                                    storageService: storageService,
+                                    notificationService: notificationService);
                                   },
                                  ),
                            ),
@@ -55,8 +68,12 @@ class BirthdaysForCalendarDayWidget extends StatelessWidget {
                 ),
                 floatingActionButton: FloatingActionButton(
                 onPressed: () {
-                  Provider.of<BirthdaysForCalendarDayManager>(context, listen: false).handleAddBirthdayBtnPressed(context, dateOfDay);
-                  },
+                  Provider.of<BirthdaysForCalendarDayManager>(context, listen: false).handleAddBirthdayBtnPressed(
+                      context,
+                      dateOfDay,
+                      storageService
+                  );
+                },
                 child: Icon(Icons.add)),
               );
           },
