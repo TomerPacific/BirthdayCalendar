0b5244863432d005ef7e0639045acaf3d093e944
unknown <tomerpacific@gmail.com>
2024-06-05 21:05:00 +0300

    BirthdayCalendar formatting

--- lib/ContactsPermissionStatusBloc/ContactsPermissionStatusBloc.dart
+++ lib/ContactsPermissionStatusBloc/ContactsPermissionStatusBloc.dart
@@ -2,15 +2,21 @@ import 'package:birthday_calendar/service/contacts_service/contacts_service.dart
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-enum ContactsPermissionStatusEvent { PermissionUnknown, PermissionDenied, PermissionGranted, PermissionPermanentlyDenied }
+enum ContactsPermissionStatusEvent {
+  PermissionUnknown,
+  PermissionDenied,
+  PermissionGranted,
+  PermissionPermanentlyDenied
+}
 
-class ContactsPermissionStatusBloc extends Bloc<ContactsPermissionStatusEvent, PermissionStatus> {
-
-  ContactsPermissionStatusBloc(ContactsService contactsService) : super(PermissionStatus.denied) {
-
+class ContactsPermissionStatusBloc
+    extends Bloc<ContactsPermissionStatusEvent, PermissionStatus> {
+  ContactsPermissionStatusBloc(ContactsService contactsService)
+      : super(PermissionStatus.denied) {
     on<ContactsPermissionStatusEvent>((event, emit) async {
       if (event == ContactsPermissionStatusEvent.PermissionUnknown) {
-        bool permissionStatus = await contactsService.isContactsPermissionsPermanentlyDenied();
+        bool permissionStatus =
+            await contactsService.isContactsPermissionsPermanentlyDenied();
         if (permissionStatus) {
           emit(PermissionStatus.permanentlyDenied);
           return;
@@ -20,8 +26,8 @@ class ContactsPermissionStatusBloc extends Bloc<ContactsPermissionStatusEvent, P
     });
   }
 
-
-  PermissionStatus _convertEventNameToPermissionStatus(ContactsPermissionStatusEvent event) {
+  PermissionStatus _convertEventNameToPermissionStatus(
+      ContactsPermissionStatusEvent event) {
     switch (event) {
       case ContactsPermissionStatusEvent.PermissionDenied:
         return PermissionStatus.denied;
