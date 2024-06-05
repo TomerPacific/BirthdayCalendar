072705e2f777b3b7d0d2e44b2cedc7e4a9999049
unknown <tomerpacific@gmail.com>
2024-06-05 22:45:00 +0300

    BirthdayCalendar formatting

--- lib/ThemeBloc/ThemeBloc.dart
+++ lib/ThemeBloc/ThemeBloc.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-
 enum ThemeEvent { toggleDark, toggleLight }
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeMode> {
-
   ThemeBloc() : super(ThemeMode.light) {
     on<ThemeEvent>((event, emit) {
       emit(event == ThemeEvent.toggleDark ? ThemeMode.dark : ThemeMode.light);
