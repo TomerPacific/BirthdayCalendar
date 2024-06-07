0342f949dabdc9c3b72f4f801fbd37a634c0f81d
unknown <tomerpacific@gmail.com>
2024-06-07 13:15:30 +0300

    BirthdayCalendar creating bloc for handling birthday notfication status changes

---
+++ lib/UserNotificationStatusBloc/UserNotificationStatusBloc.dart
@@ -0,0 +1,33 @@
+import 'package:birthday_calendar/model/user_birthday.dart';
+import 'package:birthday_calendar/service/notification_service/notification_service.dart';
+import 'package:birthday_calendar/service/storage_service/storage_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class UserNotificationStatusEvent {
+  UserNotificationStatusEvent({
+    required this.userBirthday,
+    required this.hasNotification
+  });
+
+  final UserBirthday userBirthday;
+  final bool hasNotification;
+}
+
+class UserNotificationStatusBloc extends Bloc<UserNotificationStatusEvent, bool> {
+  UserNotificationStatusBloc(StorageService storageService, NotificationService notificationService) : super(false) {
+    on<UserNotificationStatusEvent>((event, emit) async {
+        bool notificationStatus = event.hasNotification;
+        notificationStatus = !notificationStatus;
+        UserBirthday birthday = event.userBirthday;
+        birthday.hasNotification = notificationStatus;
+        storageService.updateNotificationStatusForBirthday(birthday, notificationStatus);
+        if (!notificationStatus) {
+          notificationService.cancelNotificationForBirthday(birthday);
+        } else {
+          notificationService.scheduleNotificationForBirthday(
+              birthday, "${birthday.name} has an upcoming birthday!");
+        }
+        emit(notificationStatus);
+    });
+  }
+}
