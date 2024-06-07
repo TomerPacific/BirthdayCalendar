2ed4ce2c42d1291dbc6d88a6459c05cda1cdacd9
unknown <tomerpacific@gmail.com>
2024-06-07 13:45:24 +0300

    BirthdayCalendar formatting

--- lib/page/birthday/birthday.dart
+++ lib/page/birthday/birthday.dart
@@ -8,11 +8,7 @@ import 'package:birthday_calendar/model/user_birthday.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-enum ElementType {
-  background,
-  icon,
-  text
-}
+enum ElementType { background, icon, text }
 
 class BirthdayWidget extends StatelessWidget {
   final UserBirthday birthdayOfPerson;
@@ -38,7 +34,8 @@ class BirthdayWidget extends StatelessWidget {
     return index % 2 == 0 ? Colors.white : Colors.black;
   }
 
-  void _handleCallButtonPressed(BuildContext context, String phoneNumber) async {
+  void _handleCallButtonPressed(
+      BuildContext context, String phoneNumber) async {
     Uri phoneUri = Uri.parse('tel:$phoneNumber');
     if (await canLaunchUrl(phoneUri)) {
       launchUrl(phoneUri);
@@ -49,55 +46,60 @@ class BirthdayWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-        return Container(
-          height: 40,
-          color: _getColorBasedOnPosition(indexOfBirthday, ElementType.background),
-          child: Row(
-            children: [
-              new Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  birthdayOfPerson.name,
-                  textDirection: TextDirection.ltr,
-                  style: new TextStyle(
-                      fontSize: 20.0,
-                      color: _getColorBasedOnPosition(indexOfBirthday, ElementType.text)),
-                ),
-              ),
-              new Spacer(),
-              BlocProvider(create: (context) => UserNotificationStatusBloc(storageService, notificationService),
-                child: BlocBuilder<UserNotificationStatusBloc, bool>(
-                      builder: (context, state) {
-                        return new IconButton(
-                              icon: Icon(
-                              !birthdayOfPerson.hasNotification
-                              ? Icons.notifications_off_outlined
-                                  : Icons.notifications_active_outlined,
-                              color: _getColorBasedOnPosition(indexOfBirthday, ElementType.icon)),
-                              onPressed: () {
-                                BlocProvider.of<UserNotificationStatusBloc>(context).add(
-                                      new UserNotificationStatusEvent(
-                                                userBirthday: birthdayOfPerson,
-                                                hasNotification: birthdayOfPerson.hasNotification)
-                                      );
-                                });
-                        }
-                    )
-                ),
-              if (birthdayOfPerson.phoneNumber.isNotEmpty) ...[
-                new IconButton(
-                    icon: Icon(Icons.call,
-                        color: _getColorBasedOnPosition(indexOfBirthday, ElementType.icon)),
-                    onPressed: () {
-                      _handleCallButtonPressed(context, birthdayOfPerson.phoneNumber);
-                    })
-              ],
-              new IconButton(
-                  icon: Icon(Icons.clear,
-                      color: _getColorBasedOnPosition(indexOfBirthday, ElementType.icon)),
-                  onPressed: onDeletePressedCallback),
-            ],
+    return Container(
+      height: 40,
+      color: _getColorBasedOnPosition(indexOfBirthday, ElementType.background),
+      child: Row(
+        children: [
+          new Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              birthdayOfPerson.name,
+              textDirection: TextDirection.ltr,
+              style: new TextStyle(
+                  fontSize: 20.0,
+                  color: _getColorBasedOnPosition(
+                      indexOfBirthday, ElementType.text)),
+            ),
           ),
-        );
+          new Spacer(),
+          BlocProvider(
+              create: (context) => UserNotificationStatusBloc(
+                  storageService, notificationService),
+              child: BlocBuilder<UserNotificationStatusBloc, bool>(
+                  builder: (context, state) {
+                return new IconButton(
+                    icon: Icon(
+                        !birthdayOfPerson.hasNotification
+                            ? Icons.notifications_off_outlined
+                            : Icons.notifications_active_outlined,
+                        color: _getColorBasedOnPosition(
+                            indexOfBirthday, ElementType.icon)),
+                    onPressed: () {
+                      BlocProvider.of<UserNotificationStatusBloc>(context).add(
+                          new UserNotificationStatusEvent(
+                              userBirthday: birthdayOfPerson,
+                              hasNotification:
+                                  birthdayOfPerson.hasNotification));
+                    });
+              })),
+          if (birthdayOfPerson.phoneNumber.isNotEmpty) ...[
+            new IconButton(
+                icon: Icon(Icons.call,
+                    color: _getColorBasedOnPosition(
+                        indexOfBirthday, ElementType.icon)),
+                onPressed: () {
+                  _handleCallButtonPressed(
+                      context, birthdayOfPerson.phoneNumber);
+                })
+          ],
+          new IconButton(
+              icon: Icon(Icons.clear,
+                  color: _getColorBasedOnPosition(
+                      indexOfBirthday, ElementType.icon)),
+              onPressed: onDeletePressedCallback),
+        ],
+      ),
+    );
   }
 }
