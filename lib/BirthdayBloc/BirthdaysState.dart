23de2fc122d120ce87049dca97dd6f08791c89bb
unknown <tomerpacific@gmail.com>
2024-06-10 20:16:47 +0300

    BirthdayCalenday formatting

--- lib/BirthdayBloc/BirthdaysState.dart
+++ lib/BirthdayBloc/BirthdaysState.dart
@@ -1,17 +1,10 @@
-
 import 'package:birthday_calendar/model/user_birthday.dart';
 
 class BirthdaysState {
-
   final DateTime? date;
   final List<UserBirthday>? birthdays;
   final bool showAddBirthdayDialog;
 
-  BirthdaysState({
-    this.date,
-    this.birthdays,
-    required this.showAddBirthdayDialog
-  });
-
-
+  BirthdaysState(
+      {this.date, this.birthdays, required this.showAddBirthdayDialog});
 }
