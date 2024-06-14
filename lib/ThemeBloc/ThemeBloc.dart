d438f83437c473279d55b77c1e0b98d551fe70d9
unknown <tomerpacific@gmail.com>
2024-06-14 20:05:54 +0300

    BirthdayCalenday adding logic to save and fetch saved theme mode

--- lib/ThemeBloc/ThemeBloc.dart
+++ lib/ThemeBloc/ThemeBloc.dart
@@ -1,12 +1,15 @@
+import 'package:birthday_calendar/service/storage_service/storage_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 enum ThemeEvent { toggleDark, toggleLight }
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeMode> {
-  ThemeBloc() : super(ThemeMode.light) {
+  ThemeBloc(StorageService storageService, bool isDarkMode) : super(isDarkMode ? ThemeMode.dark : ThemeMode.light) {
     on<ThemeEvent>((event, emit) {
-      emit(event == ThemeEvent.toggleDark ? ThemeMode.dark : ThemeMode.light);
+      ThemeMode themeMode = event == ThemeEvent.toggleDark ? ThemeMode.dark : ThemeMode.light;
+      emit(themeMode);
+      storageService.saveThemeModeSetting(themeMode == ThemeMode.dark ? true : false);
     });
   }
 }
