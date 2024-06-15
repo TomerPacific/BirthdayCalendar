347382bcd76a5b811ee168cc8ba21235e70156a4
unknown <tomerpacific@gmail.com>
2024-06-15 13:28:16 +0300

    BirthdayCalenday removing date service as a dependency and using util class instead

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:birthday_calendar/ContactsPermissionStatusBloc/ContactsPermissionStatusBloc.dart';
+import 'package:birthday_calendar/BirthdayCalendarDateUtils.dart';
 import 'package:birthday_calendar/ThemeBloc/ThemeBloc.dart';
 import 'package:birthday_calendar/VersionBloc/VersionBloc.dart';
 import 'package:birthday_calendar/service/contacts_service/contacts_service.dart';
 import 'package:birthday_calendar/service/contacts_service/contacts_service_impl.dart';
-import 'package:birthday_calendar/service/date_service/date_service_impl.dart';
 import 'package:birthday_calendar/service/notification_service/notification_service.dart';
 import 'package:birthday_calendar/service/notification_service/notification_service_impl.dart';
 import 'package:birthday_calendar/service/permission_service/permissions_service.dart';
@@ -12,18 +12,17 @@ import 'package:birthday_calendar/service/storage_service/shared_preferences_sto
 import 'package:birthday_calendar/service/storage_service/storage_service.dart';
 import 'package:flutter/material.dart';
 import 'constants.dart';
-import 'service/date_service/date_service.dart';
 import 'package:birthday_calendar/page/main_page/main_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  DateService dateService = DateServiceImpl();
   NotificationService notificationService = NotificationServiceImpl();
   PermissionsService permissionsService = PermissionsServiceImpl();
   StorageService storageService =
-      StorageServiceSharedPreferences(dateService: dateService);
+      StorageServiceSharedPreferences();
   ContactsService contactsService = ContactsServiceImpl(
       storageService: storageService,
       notificationService: notificationService,
@@ -35,7 +34,6 @@ Future<void> main() async {
       notificationService: notificationService,
       contactsService: contactsService,
       storageService: storageService,
-      dateService: dateService,
       isDarkMode: isDarkMode,
   ));
 }
@@ -45,13 +43,11 @@ class MyApp extends StatelessWidget {
       {required this.notificationService,
       required this.contactsService,
       required this.storageService,
-      required this.dateService,
       required this.isDarkMode});
 
   final NotificationService notificationService;
   final ContactsService contactsService;
   final StorageService storageService;
-  final DateService dateService;
   final bool isDarkMode;
 
   @override
@@ -75,9 +71,8 @@ class MyApp extends StatelessWidget {
                   notificationService: notificationService,
                   contactsService: contactsService,
                   storageService: storageService,
-                  dateService: dateService,
                   title: applicationName,
-                  currentMonth: dateService.getCurrentMonthNumber()));
+                  currentMonth: BirthdayCalendarDateUtils.getCurrentMonthNumber()));
         },
       ),
     );
