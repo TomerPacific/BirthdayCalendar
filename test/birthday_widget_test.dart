1795f81c33758db7bd97f7841f0b93ba7d979e32
unknown <tomerpacific@gmail.com>
2024-07-27 13:25:02 +0300

    BirthdayCalendar refactoring tests to use a base widget

--- test/birthday_widget_test.dart
+++ test/birthday_widget_test.dart
@@ -1,8 +1,13 @@
+import 'package:birthday_calendar/BirthdayBloc/BirthdaysBloc.dart';
+import 'package:birthday_calendar/BirthdayBloc/BirthdaysState.dart';
+import 'package:birthday_calendar/ThemeBloc/ThemeBloc.dart';
+import 'package:birthday_calendar/VersionBloc/VersionBloc.dart';
 import 'package:birthday_calendar/model/user_birthday.dart';
 import 'package:birthday_calendar/service/notification_service/notification_service.dart';
 import 'package:birthday_calendar/service/notification_service/notification_service_impl.dart';
 import 'package:birthday_calendar/service/storage_service/shared_preferences_storage.dart';
 import 'package:birthday_calendar/service/storage_service/storage_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:birthday_calendar/page/birthday/birthday.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +20,7 @@ void print(String s) => printLog.add(s);
 void main() {
   StorageService storageService = StorageServiceSharedPreferences();
   NotificationService notificationService = NotificationServiceImpl();
+  List<UserBirthday> birthdays = [];
 
   setUp(() {
     return Future(() async {
@@ -23,52 +29,86 @@ void main() {
     });
   });
 
+  Widget base = MultiBlocProvider(
+    providers: [
+      BlocProvider(create: (context) => ThemeBloc(storageService, false)),
+      BlocProvider(create: (context) => VersionBloc())
+    ],
+    child: BlocBuilder<ThemeBloc, ThemeMode>(
+      builder: (context, state) {
+        return MaterialApp(
+            title: '',
+            theme: ThemeData.light(),
+            themeMode: state,
+            darkTheme: ThemeData.dark(),
+            home: Material(
+                child: new SizedBox(
+                    height: 40,
+                    child: BlocProvider(
+                        create: (context) => BirthdaysBloc(
+                            notificationService, storageService, birthdays),
+                        child: BlocBuilder<BirthdaysBloc, BirthdaysState>(
+                            builder: (context, state) {
+                              return Column(children: [
+                                (state.birthdays == null ||
+                                    state.birthdays!.length == 0)
+                                    ? Spacer()
+                                    : Expanded(
+                                  child: ListView.builder(
+                                    itemCount: state.birthdays != null
+                                        ? state.birthdays!.length
+                                        : 0,
+                                    itemBuilder:
+                                        (BuildContext context, int index) {
+                                      return BlocProvider.value(
+                                          value: BlocProvider.of<BirthdaysBloc>(
+                                              context),
+                                          child: BirthdayWidget(
+                                              key: Key(
+                                                  state.birthdays![index].name),
+                                              birthdayOfPerson:
+                                              state.birthdays![index],
+                                              indexOfBirthday: index,
+                                              storageService: storageService,
+                                              notificationService:
+                                              notificationService));
+                                    },
+                                  ),
+                                ),
+                              ]);
+                            }))))
+        );
+      },
+    ),
+  );
+
   testWidgets("BirthdayWidget show birthday for Someone",
       (WidgetTester tester) async {
     final String phoneNumber = '+234 500 500 5005';
     UserBirthday userBirthday =
         new UserBirthday("Someone", DateTime.now(), false, phoneNumber);
-
-    await tester.pumpWidget(MaterialApp(
-        home: Material(
-            child: new SizedBox(
-                height: 40,
-                child: BirthdayWidget(
-                    key: Key("123"),
-                    birthdayOfPerson: userBirthday,
-                    indexOfBirthday: 1,
-                    storageService: storageService,
-                    notificationService: notificationService)))));
+    birthdays = [userBirthday];
+    await tester.pumpWidget(base);
 
     final nameFinder = find.text('Someone');
     expect(nameFinder, findsOneWidget);
   });
 
-  testWidgets("BirthdayWidget click on remove notification icon",
+  testWidgets("BirthdayWidget click on remove birthday icon",
       (WidgetTester tester) async {
     final String phoneNumber = '+234 500 500 5005';
     UserBirthday userBirthday =
         new UserBirthday("Someone", DateTime.now(), false, phoneNumber);
-
-    await tester.pumpWidget(MaterialApp(
-        home: Material(
-            child: new SizedBox(
-                height: 40,
-                child: BirthdayWidget(
-                    key: Key("123"),
-                    birthdayOfPerson: userBirthday,
-                    indexOfBirthday: 1,
-                    storageService: storageService,
-                    notificationService: notificationService)))));
+    birthdays = [userBirthday];
+    await tester.pumpWidget(base);
 
     await tester.tap(find.descendant(
         of: find.byType(IconButton), matching: find.byIcon(Icons.clear)));
 
-    await tester.pump();
+    await tester.pumpAndSettle();
 
-    expect(printLog.length, 1);
-    bool sawDeletedMessage = printLog[0].toString().contains('Deleted');
-    expect(sawDeletedMessage, true);
+    final nameFinder = find.text('Someone');
+    expect(nameFinder, findsNothing);
   });
 
   testWidgets("BirthdayWidget press on call button",
@@ -77,16 +117,9 @@ void main() {
     UserBirthday userBirthday =
         new UserBirthday("Someone", DateTime.now(), false, phoneNumber);
 
-    await tester.pumpWidget(MaterialApp(
-        home: Material(
-            child: new SizedBox(
-                height: 40,
-                child: BirthdayWidget(
-                    key: Key("123"),
-                    birthdayOfPerson: userBirthday,
-                    indexOfBirthday: 1,
-                    storageService: storageService,
-                    notificationService: notificationService)))));
+    birthdays = [userBirthday];
+
+    await tester.pumpWidget(base);
 
     await tester.tap(find.descendant(
         of: find.byType(IconButton), matching: find.byIcon(Icons.call)));
