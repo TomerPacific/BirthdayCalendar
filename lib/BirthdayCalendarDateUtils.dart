07bec3d34ff6e860443bd18c372a5a445717bd97
unknown <tomerpacific@gmail.com>
2024-11-01 22:34:09 +0200

    BirthdayCalendar adding method to translate month by number

--- lib/BirthdayCalendarDateUtils.dart
+++ lib/BirthdayCalendarDateUtils.dart
@@ -1,8 +1,7 @@
-
-
-
 import 'package:birthday_calendar/constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class BirthdayCalendarDateUtils {
 
@@ -91,4 +90,35 @@ class BirthdayCalendarDateUtils {
 
     return isValidDate;
   }
+
+  static String translateMonth(int month, BuildContext context) {
+         switch (month) {
+            case 1:
+              return AppLocalizations.of(context)!.january;
+            case 2:
+              return AppLocalizations.of(context)!.february;
+            case 3:
+              return AppLocalizations.of(context)!.march;
+            case 4:
+              return AppLocalizations.of(context)!.april;
+            case 5:
+              return AppLocalizations.of(context)!.may;
+            case 6:
+              return AppLocalizations.of(context)!.june;
+            case 7:
+              return AppLocalizations.of(context)!.july;
+            case 8:
+              return AppLocalizations.of(context)!.august;
+            case 9:
+              return AppLocalizations.of(context)!.september;
+            case 10:
+              return AppLocalizations.of(context)!.october;
+            case 11:
+              return AppLocalizations.of(context)!.november;
+            case 12:
+              return AppLocalizations.of(context)!.december;
+            default:
+              return "";
+          }
+    }
 }
