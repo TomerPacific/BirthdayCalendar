86496f2050c2aa39dabe4cf18e7c5dcaa0b447f5
unknown <tomerpacific@gmail.com>
2024-11-02 11:15:06 +0200

    BirthdayCalendar removing unused constants and creating constants for month numbers

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,18 +1,18 @@
 const String applicationName = "Birthday Calendar";
-const Map<int, String> months = const {
-  1: "January",
-  2: "February",
-  3: "March",
-  4: "April",
-  5: 'May',
-  6: "June",
-  7: "July",
-  8: "August",
-  9: "September",
-  10: "October",
-  11: "November",
-  12: "December"
-};
+
+const JANUARY_MONTH_NUMBER = 1;
+const FEBRUARY_MONTH_NUMBER = 2;
+const MARCH_MONTH_NUMBER = 3;
+const APRIL_MONTH_NUMBER = 4;
+const MAY_MONTH_NUMBER = 5;
+const JUNE_MONTH_NUMBER = 6;
+const JULY_MONTH_NUMBER = 7;
+const AUGUST_MONTH_NUMBER = 8;
+const SEPTEMBER_MONTH_NUMBER = 9;
+const OCTOBER_MONTH_NUMBER = 10;
+const NOVEMBER_MONTH_NUMBER = 11;
+const DECEMBER_MONTH_NUMBER = 12;
+
 const addBirthday = "Add Birthday";
 const userBirthdayNameKey = "name";
 const userBirthdayDateKey = "birthdayDate";
