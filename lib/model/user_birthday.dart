d19da2a811245de9095a3e1837eca03f1b352c77
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-05-29 14:29:46 +0300

    BirthdayCalendar using notification id instead of hash code

--- lib/model/user_birthday.dart
+++ lib/model/user_birthday.dart
@@ -5,9 +5,13 @@ class UserBirthday {
   final DateTime birthdayDate;
   bool hasNotification;
   String phoneNumber;
+  final int notificationId;
 
   UserBirthday(
-      this.name, this.birthdayDate, this.hasNotification, this.phoneNumber);
+      this.name, this.birthdayDate, this.hasNotification, this.phoneNumber,
+      {int? notificationId})
+      : this.notificationId = notificationId ??
+            (name.hashCode ^ birthdayDate.hashCode).toUnsigned(31);
 
   void updateNotificationStatus(bool status) {
     this.hasNotification = status;
@@ -34,12 +38,19 @@ class UserBirthday {
         birthdayDate =
             DateTime.tryParse(json[userBirthdayDateKey]) ?? DateTime.now(),
         hasNotification = json[userBirthdayHasNotificationKey],
-        phoneNumber = json[userBirthdayPhoneNumberKey];
+        phoneNumber = json[userBirthdayPhoneNumberKey],
+        notificationId = json[userBirthdayNotificationIdKey] ??
+            (json[userBirthdayNameKey].hashCode ^
+                    (DateTime.tryParse(json[userBirthdayDateKey]) ??
+                            DateTime.now())
+                        .hashCode)
+                .toUnsigned(31);
 
   Map<String, dynamic> toJson() => {
         userBirthdayNameKey: name,
         userBirthdayDateKey: birthdayDate.toIso8601String(),
         userBirthdayHasNotificationKey: hasNotification,
-        userBirthdayPhoneNumberKey: phoneNumber
+        userBirthdayPhoneNumberKey: phoneNumber,
+        userBirthdayNotificationIdKey: notificationId
       };
 }
