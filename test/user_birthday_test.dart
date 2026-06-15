d8b2b09ac4abb4883cc926a38be5941a33a76285
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-06-15 22:50:02 +0300

    BirthdayCalendar adding more tests

--- test/user_birthday_test.dart
+++ test/user_birthday_test.dart
@@ -44,4 +44,48 @@ void main() {
       expect(!userBirthday.birthdayDate.isAfter(after), isTrue);
     });
   });
+
+  group('UserBirthday equality and hashCode', () {
+    test('same name, month, day but different year should be equal', () {
+      final birthday1 = UserBirthday('Alice', DateTime(1990, 6, 15), false, '');
+      final birthday2 = UserBirthday('Alice', DateTime(2000, 6, 15), true, '123');
+
+      expect(birthday1 == birthday2, isTrue);
+      expect(birthday1.hashCode, equals(birthday2.hashCode));
+    });
+
+    test('different name should not be equal', () {
+      final birthday1 = UserBirthday('Alice', DateTime(1990, 6, 15), false, '');
+      final birthday2 = UserBirthday('Bob', DateTime(1990, 6, 15), false, '');
+
+      expect(birthday1 == birthday2, isFalse);
+      expect(birthday1.hashCode, isNot(equals(birthday2.hashCode)));
+    });
+
+    test('different month should not be equal', () {
+      final birthday1 = UserBirthday('Alice', DateTime(1990, 6, 15), false, '');
+      final birthday2 = UserBirthday('Alice', DateTime(1990, 7, 15), false, '');
+
+      expect(birthday1 == birthday2, isFalse);
+      expect(birthday1.hashCode, isNot(equals(birthday2.hashCode)));
+    });
+
+    test('different day should not be equal', () {
+      final birthday1 = UserBirthday('Alice', DateTime(1990, 6, 15), false, '');
+      final birthday2 = UserBirthday('Alice', DateTime(1990, 6, 16), false, '');
+
+      expect(birthday1 == birthday2, isFalse);
+      expect(birthday1.hashCode, isNot(equals(birthday2.hashCode)));
+    });
+
+    test('identical objects should be equal', () {
+      final birthday = UserBirthday('Alice', DateTime(1990, 6, 15), false, '');
+      expect(birthday == birthday, isTrue);
+    });
+
+    test('comparison with different type should be false', () {
+      final birthday = UserBirthday('Alice', DateTime(1990, 6, 15), false, '');
+      expect(birthday == 'Alice', isFalse);
+    });
+  });
 }
