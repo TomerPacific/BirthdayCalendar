a85d6d6f7ea9a1aee0f47f52be5fa36b46f3326b
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-06-20 09:51:33 +0300

    BirthdayCalendar refactoring clear notifications bloc

--- lib/ClearNotificationsBloc/ClearNotificationsBloc.dart
+++ lib/ClearNotificationsBloc/ClearNotificationsBloc.dart
@@ -2,21 +2,27 @@ import 'package:birthday_calendar/service/notification_service/notification_serv
 import 'package:birthday_calendar/service/storage_service/storage_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-enum ClearNotificationsEvent { ClearedNotifications }
+abstract class ClearNotificationsEvent {}
 
-class ClearNotificationsBloc extends Bloc<ClearNotificationsEvent, bool> {
+class ClearNotificationsRequested extends ClearNotificationsEvent {}
 
+sealed class ClearNotificationsState {}
+
+class ClearNotificationsInitial extends ClearNotificationsState {}
+
+class ClearNotificationsCompleted extends ClearNotificationsState {}
+
+class ClearNotificationsBloc
+    extends Bloc<ClearNotificationsEvent, ClearNotificationsState> {
   final StorageService storageService;
   final NotificationService notificationService;
 
   ClearNotificationsBloc(this.storageService, this.notificationService)
-      : super(false) {
-    on<ClearNotificationsEvent>((event, emit) async {
-      if (event == ClearNotificationsEvent.ClearedNotifications) {
-        await storageService.clearAllBirthdays();
-        await notificationService.cancelAllNotifications();
-        emit(true);
-      }
+      : super(ClearNotificationsInitial()) {
+    on<ClearNotificationsRequested>((event, emit) async {
+      await storageService.clearAllBirthdays();
+      await notificationService.cancelAllNotifications();
+      emit(ClearNotificationsCompleted());
     });
   }
 }
